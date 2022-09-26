a09a2192d02757a156172232170e3e0b73659ce1
Fernando01santana <cool.fernando23@gmail.com>
2022-09-26 08:11:21 -0300

    feat:base api

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const data = require('./data/info.json') 
+
+app.get('/', (req, res) => {
+  res.send(data)
+})
+ 
+app.listen(port, () => {
+  console.log(`SERVIDOR ON http://localhost:${port}`)
+});
