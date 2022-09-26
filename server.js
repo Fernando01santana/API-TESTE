9c3279d3546951c20a5cd663fc9780cb2194025c
Fernando01santana <cool.fernando23@gmail.com>
2022-09-26 08:18:05 -0300

    feat:alter port

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const data = require('./data/info.json') 
 
 app.get('/categories', (req, res) => {
   res.send(data)
 })
  
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000,()=>{ 
   console.log(`SERVIDOR ON http://localhost:${port}`)
 });
